c9b6e72e8bef8dbccc58257abc912715809542ae
Oscar Cantu <omcantu@gmail.com>
2025-09-20 22:22:46 -0500

    using kwriteconfig for default terminal

--- install/desktop/set-alacritty-default.sh
+++ install/desktop/set-alacritty-default.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-# For KDE Plasma 6, use the following to set the default terminal:
-plasma-settings set default-terminal alacritty.desktop
+source ~/.local/share/omakub/install/lib/env.sh
+$KWRC --file kdeglobals --group General --key TerminalApplication alacritty.desktop
