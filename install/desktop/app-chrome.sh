eff28780cdbf50c7a1a2de779e63adc40db0e366
Oscar Cantu <omcantu@gmail.com>
2025-09-21 00:25:20 -0500

    fix default chrome

--- install/desktop/app-chrome.sh
+++ install/desktop/app-chrome.sh
@@ -5,6 +5,5 @@ cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
-# For KDE Plasma 6, use the following to set the default browser:
-plasma-settings set default-browser google-chrome.desktop
+
 cd -
