a95dec7a6e06bdd0ae63f55363fe0102cec57d99
Oscar Cantu <omcantu@gmail.com>
2025-09-22 15:38:27 -0500

    fixing plasma backend package name difference in fedora vs ubuntu

--- install/desktop/a-flatpak.sh
+++ install/desktop/a-flatpak.sh
@@ -2,5 +2,9 @@
 source ~/.local/share/omakub/install/lib/env.sh
 
 sudo $PKG_MGR install -y flatpak
-sudo $PKG_MGR install -y plasma-discover-backend-flatpak
+if [ "$OS_NAME" = "Ubuntu" ]; then
+  sudo $PKG_MGR install -y plasma-discover-backend-flatpak
+else
+  sudo $PKG_MGR install -y plasma-discover-flatpak
+fi
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
