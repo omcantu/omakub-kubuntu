079ce3b6d772eaa5489250b184afcf4597336a15
Oscar Cantu <omcantu@gmail.com>
2025-09-22 11:21:59 -0500

    wrappint ubuntu for fastfetch

--- install/terminal/app-fastfetch.sh
+++ install/terminal/app-fastfetch.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 source ~/.local/share/omakub/install/lib/env.sh
 # Display system information in the terminal
-sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
+
+if [ "$OS_NAME" = "Ubuntu" ]; then
+  sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
+fi
 sudo $PKG_MGR update -y
 sudo $PKG_MGR install -y fastfetch
 
