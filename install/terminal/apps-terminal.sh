9d4a40f71fbb3899e0a0eb8dadfab0b7f9f4adec
Oscar Cantu <omcantu@gmail.com>
2025-09-22 11:57:39 -0500

    fedora doesn't have apache2-utils, it has httpd-tools

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 source ~/.local/share/omakub/install/lib/env.sh
-sudo $PKG_MGR install -y fzf ripgrep bat eza zoxide plocate apache2-utils fd-find tldr
+
+if [ "$OS_NAME" = "Ubuntu" ]; then
+  sudo $PKG_MGR install -y fzf ripgrep bat eza zoxide plocate apache2-utils fd-find tldr
+else
+  sudo $PKG_MGR install -y fzf ripgrep bat eza zoxide plocate httpd-tools fd-find tldr
+fi
+
