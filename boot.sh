a831b918250e5ba23007e7ea022ebf81275f9126
Oscar Cantu <omcantu@gmail.com>
2025-09-22 10:54:50 -0500

    fixing order of operations for boot.sh

--- boot.sh
+++ boot.sh
@@ -14,15 +14,16 @@ echo -e "$ascii_art"
 echo "=> Omakub is for fresh Ubuntu 24.04+ installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
-echo "Cloning Omakub..."
-rm -rf ~/.local/share/omakub
-git clone https://github.com/omcantu/omakub-kubuntu.git ~/.local/share/omakub >/dev/null
-
 # Load environment helpers
 source ~/.local/share/omakub/install/lib/env.sh
 
 sudo $PKG_MGR update >/dev/null
 sudo $PKG_MGR install -y git >/dev/null
+
+echo "Cloning Omakub..."
+rm -rf ~/.local/share/omakub
+git clone https://github.com/omcantu/omakub-kubuntu.git ~/.local/share/omakub >/dev/null
+
 
 echo "Installation starting..."
 source ~/.local/share/omakub/install.sh
