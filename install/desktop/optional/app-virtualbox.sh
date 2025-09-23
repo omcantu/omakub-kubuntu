fb6f6b3892c15f9b3f01970e0d3604b3b7eef003
Oscar Cantu <omcantu@gmail.com>
2025-09-22 21:49:47 -0500

    fixing virtualbox

--- install/desktop/optional/app-virtualbox.sh
+++ install/desktop/optional/app-virtualbox.sh
@@ -4,5 +4,11 @@ source ~/.local/share/omakub/install/lib/env.sh
 # See https://ubuntu.com/tutorials/how-to-run-ubuntu-desktop-on-a-virtual-machine-using-virtualbox#1-overview
 # for a guide on how to run Ubuntu inside it.
 
-sudo $PKG_MGR install -y virtualbox virtualbox-ext-pack
+if [ "$OS_NAME" = "Ubuntu" ]; then
+  sudo $PKG_MGR install -y virtualbox virtualbox-ext-pack
+else
+  sudo $PKG_MGR install -y VirtualBox VirtualBox-guest-additions
+fi
 sudo usermod -aG vboxusers ${USER}
+
+
