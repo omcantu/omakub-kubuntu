95dcc3f2e1e73bbdf20bfb7bcb8731ed85db404e
Oscar Cantu <omcantu@gmail.com>
2025-09-23 23:05:49 -0500

    fedora running clean all for obs-studio since signature has failed twice before.

--- install/desktop/optional/app-obs-studio.sh
+++ install/desktop/optional/app-obs-studio.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 source ~/.local/share/omakub/install/lib/env.sh
 # OBS Studio is a screen recording application that allows you to capture both display and webcam in the same recording
+if [ "$OS_NAME" = "Fedora" ]; then
+  sudo $PKG_MGR clean all
+fi
 sudo $PKG_MGR install -y obs-studio
