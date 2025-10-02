a3a992ab7dff3806886ae7be604ed666d9dd73d6
Oscar M. Cantu <omcantu@gmail.com>
2025-10-01 21:40:07 -0500

    Update app-pinta.sh

--- install/desktop/app-pinta.sh
+++ install/desktop/app-pinta.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 source ~/.local/share/omakub/install/lib/env.sh
 # Pinta is a simple yet powerful image editor. See https://www.pinta-project.com/
-if [ "$OS_NAME" = "Ubuntu" ]; then
-  # FIXME: Get this out of snap
-  sudo snap install pinta
-else
-  flatpak install -y flathub com.github.PintaProject.Pinta
-fi
+flatpak install -y flathub com.github.PintaProject.Pinta
