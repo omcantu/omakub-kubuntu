1062ccb3a69711900f7de0421aa21316adaa2e65
Oscar M. Cantu <omcantu@gmail.com>
2025-10-02 22:51:07 -0500

    Update set-alacritty-default.sh

--- install/desktop/set-alacritty-default.sh
+++ install/desktop/set-alacritty-default.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 source ~/.local/share/omakub/install/lib/env.sh
-$KWRC --file kdeglobals --group General --key TerminalApplication alacritty.desktop
+#$KWRC --file kdeglobals --group General --key TerminalApplication alacritty.desktop
