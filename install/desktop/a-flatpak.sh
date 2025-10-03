f214710682758c3225f0a05e5196b5b4caba9662
Oscar M. Cantu <omcantu@gmail.com>
2025-10-02 22:45:18 -0500

    Update a-flatpak.sh

--- install/desktop/a-flatpak.sh
+++ install/desktop/a-flatpak.sh
@@ -5,6 +5,6 @@ sudo $PKG_MGR install -y flatpak
 if [ "$OS_NAME" = "Ubuntu" ]; then
   sudo $PKG_MGR install -y plasma-discover-backend-flatpak
 else
-  sudo $PKG_MGR install -y plasma-discover-flatpak
+#  sudo $PKG_MGR install -y plasma-discover-flatpak
 fi
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
