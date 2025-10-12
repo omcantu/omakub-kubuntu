54f17ef8961556f3cce9b949e47645fe5f2c319f
Oscar M. Cantu <omcantu@gmail.com>
2025-10-12 09:31:55 -0500

    Update app-obsidian.sh

--- install/desktop/app-obsidian.sh
+++ install/desktop/app-obsidian.sh
@@ -2,4 +2,4 @@
 
 # Obsidian is a multi-platform note taking application. See https://obsidian.md
 gum spin --spinner meter --title "Obsidian installation about to start. It may take up to 20 minutes on some systems!" -- sleep 3
-flatpak install -y flathub md.obsidian.Obsidian
+flatpak install -y --user flathub md.obsidian.Obsidian
