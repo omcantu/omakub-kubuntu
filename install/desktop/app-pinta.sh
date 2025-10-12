2c5880cbb6448680861e32d3e5e6e6fdd7c54c13
Oscar M. Cantu <omcantu@gmail.com>
2025-10-12 09:32:28 -0500

    Update app-pinta.sh

--- install/desktop/app-pinta.sh
+++ install/desktop/app-pinta.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/.local/share/omakub/install/lib/env.sh
 # Pinta is a simple yet powerful image editor. See https://www.pinta-project.com/
-flatpak install -y flathub com.github.PintaProject.Pinta
+flatpak install -y --user flathub com.github.PintaProject.Pinta
